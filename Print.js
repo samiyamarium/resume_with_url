7a5c8e54903631bb82aa764bb04590ce1d38cf74
samiyamarium <160397803+samiyamarium@users.noreply.github.com>
2024-09-25 21:41:07 +0500

    Add files via upload

---
+++ Print.js
@@ -0,0 +1,11 @@
+function Print() {
+    var x = document.getElementById("mile3");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length; i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("demo").innerHTML = text;
+    var url = document.URL;
+    document.getElementById("demo1").innerHTML = url;
+}
